08b499c62d4eb541b48f0e5d6624b01ae9693495
Philipp Heller <10487949+hellerphilipp@users.noreply.github.com>
2025-06-12 13:53:08 +0200

    Expose S3 file transfer models from .transfer

--- documentextractor_commons/models/transfer/__init__.py
+++ documentextractor_commons/models/transfer/__init__.py
@@ -1,9 +1,12 @@
-from .file import FileResponse
+from .file import CreateFileRequest, UploadRequest, UploadResponse, FileResponse
 from .run import FileExtractionResult, RunCreate, RunResponse, RunResult
 from .schema import AttributeTypeCreate, SchemaCreate, SchemaResponse
 from .workflow import WorkflowCreate, WorkflowUpdate, WorkflowResponse
 
 __all__ = [
+    "CreateFileRequest",
+    "UploadRequest",
+    "UploadResponse",
     "FileResponse",
     "FileExtractionResult",
     "RunCreate",
